68343906006a7f8c44fae8d8ecdecc627d41d97d
fahimforhad496 <fahimforhad496@gmail.com>
2021-07-18 02:00:56 +0600

    server side data modeling, routing and service CRUD done

--- server/src/models/data-models/index.js
+++ server/src/models/data-models/index.js
@@ -1,9 +1,16 @@
 const User = require('./user');
 const Role = require('./role');
-
+const Customer = require('./customer');
+const Product = require('./product');
+const Resource = require('./resource');
+const Permission = require('./permission');
 const models = {
     User,
-    Role
+    Role,
+    Customer,
+    Product,
+    Resource,
+    Permission
 }
 
 module.exports = models;
